af317d1fee9b2ed03212962a48915cfcdece5abf
Elad Cohen <eladco13@gmail.com>
2023-07-29 17:29:11 +0300

    Update DataGeneration.py

--- DataGeneration.py
+++ DataGeneration.py
@@ -3,13 +3,13 @@ import random
 import requests
 
 # Replace with your ThingSpeak API Key and Field ID
-api_key = "HMMO4N3BX13SPUCB"
+api_key = ""
 field_id = "1"
 
 url = f"https://api.thingspeak.com/update?api_key={api_key}"
 
 while True:
-    gas_level = random.uniform(0.0, 100.0)  # Simulate gas level
+    gas_level = random.uniform(85.0, 400.0)  # Simulate gas level
     payload = {field_id: gas_level}
     response = requests.post(url, data=payload)
     print(f"Data sent to ThingSpeak: {response.status_code}")
